ce3f33606d7ae9f8c6e5ff20b6efc16ea7a59bf7
Nikita Vaulin <mr.vau2017@mail.ru>
2025-05-04 16:13:04 +0500

    refactor: add interface of service

--- QuizRuLet.Application/Services/LearningModuleService.cs
+++ QuizRuLet.Application/Services/LearningModuleService.cs
@@ -6,32 +6,28 @@ using QuizRuLet.Core.Models;
 
 namespace QuizRuLet.Application.Services;
 
-public class LearningModuleService
+public class LearningModuleService : ILearningModuleService
 {
-    private readonly IModulesRepository _modulesRepository;
-    private readonly IModuleService _moduleService;
     private readonly ICardsRepository _cardsRepository;
-    
-    public LearningModuleService(IModulesRepository modulesRepository, ICardsRepository cardsRepository, IModuleService moduleService)
+
+    public LearningModuleService(ICardsRepository cardsRepository)
     {
-        _modulesRepository = modulesRepository;
         _cardsRepository = cardsRepository;
-        _moduleService = moduleService;
     }
-    
+
     public async Task<Guid> UpdateLearningFlag(Guid cardId, bool isLearned)
     {
         return await _cardsRepository.UpdateLearningFlag(cardId, isLearned);
     }
-        
+
     public async Task<List<Card>> GetCards(Guid moduleId, bool isLearned)
     {
         return await _cardsRepository.GetByLearningFlag(moduleId, isLearned);
     }
-    
+
     public async Task<List<Card>> GetAllCards(Guid moduleId)
     {
         return await _cardsRepository.GetByModule(moduleId);
     }
-    
+
 }
---
+++ QuizRuLet.Core/Abstractions/ILearningModuleService.cs
@@ -0,0 +1,11 @@
+using QuizRuLet.Core.Models;
+
+
+namespace QuizRuLet.Core.Abstractions;
+
+public interface ILearningModuleService
+{
+    Task<List<Card>> GetAllCards(Guid moduleId);
+    Task<List<Card>> GetCards(Guid moduleId, bool isLearned);
+    Task<Guid> UpdateLearningFlag(Guid cardId, bool isLearned);
+}
